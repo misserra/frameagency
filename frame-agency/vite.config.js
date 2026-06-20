609841e48a05a7695bef27e881ddedf72de31922
Daniil <dan-kuz201@yandex.ru>
2026-06-20 22:51:35 +0300

    modified:   gh-pages

--- frame-agency/vite.config.js
+++ frame-agency/vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
   ],
+  base: '/frameagency/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
